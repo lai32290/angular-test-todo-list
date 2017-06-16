309c96b23dd7044648de419717d1b1c277ee94dd
Lai <lai32690@gmail.com>
2017-06-16 00:36:31 -0300

    add test to form text bind

--- src/app/todo-list-form/todo-list-form.component.spec.ts
+++ src/app/todo-list-form/todo-list-form.component.spec.ts
@@ -3,25 +3,46 @@ import { FormsModule } from '@angular/forms';
 
 import { TodoListFormComponent } from './todo-list-form.component';
 import { AppModule } from '../app.module';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from "@angular/core/src/debug/debug_node";
 
 describe('TodoListFormComponent', () => {
-  let component: TodoListFormComponent;
-  let fixture: ComponentFixture<TodoListFormComponent>;
+    let component: TodoListFormComponent;
+    let fixture: ComponentFixture<TodoListFormComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [ FormsModule, AppModule ]
-    })
-    .compileComponents();
-  }));
+    let de: DebugElement;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TodoListFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [ FormsModule, AppModule ]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TodoListFormComponent);
+        component = fixture.componentInstance;
+        de = fixture.debugElement.query(By.css('form'));
+
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('should bind search text', () => {
+        component.description = 'some description';
+        fixture.detectChanges();
+
+        let el = de.query(By.css('input[name=description]'));
+
+        fixture.whenStable()
+            .then(() => {
+                expect(el.nativeElement.value).toBe('some description');
+            });
+    });
+
+
+
 });
