b5c38167471e006ed4e7d6085a0988d0991233d8
Lai <lai32690@gmail.com>
2017-06-18 17:37:49 -0300

    add test to todo-service

--- src/app/todo.service.spec.ts
+++ src/app/todo.service.spec.ts
@@ -1,15 +1,43 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
+import { TodoItem } from "./todo-item";
 
 describe('TodoService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [TodoService]
-    });
-  });
+    let list: TodoItem[];
 
-  it('should ...', inject([TodoService], (service: TodoService) => {
-    expect(service).toBeTruthy();
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [TodoService]
+        });
+
+        list = [];
+    });
+    
+    it('should add new item when addItem function is called', inject([TodoService], (service: TodoService) => {
+        let description = 'A new Item';
+        service.addItem(description);
+        service.list
+            .subscribe(_list => {
+                list = _list;
+            });
+        
+        let itemInList = list.some(item => {
+            return item.description == description;
+        });
+        expect(itemInList).toBeTruthy();
+    }));
+
+    it('should to change item done states when checkDone function is called', inject([TodoService], (service: TodoService) => {
+        let description = 'Another new Item';
+        service.addItem(description);
+
+        service.list
+            .subscribe(_list => {
+                list = _list;
+            });
+
+        service.checkDone(list[0], true);
+        expect(list[0].done).toBeTruthy();
+    }));
 });
