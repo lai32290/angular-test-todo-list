b4f8657471da1666a5406a36490c63f57e2f4b57
Lai <lai32690@gmail.com>
2017-06-20 22:14:53 -0300

    add test to done pipe

--- src/app/done.pipe.spec.ts
+++ src/app/done.pipe.spec.ts
@@ -1,8 +1,30 @@
 import { DonePipe } from './done.pipe';
+import { TodoItem } from "./todo-item";
 
 describe('DonePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DonePipe();
-    expect(pipe).toBeTruthy();
-  });
+    let pipe: DonePipe;
+
+    beforeEach(() => {
+        pipe = new DonePipe();
+    });
+
+    it('create an instance', () => {
+        expect(pipe).toBeTruthy();
+    });
+
+    it('should can filter item', () => {
+        let list: TodoItem[] = [
+            new TodoItem('Item 1'),
+            new TodoItem('Item 2', true),
+            new TodoItem('Item 3'),
+            new TodoItem('Item 4')
+        ];
+
+        let doneList: TodoItem[] = pipe.transform(list, true);
+        let doingList: TodoItem[] = pipe.transform(list, false);
+
+        expect(doneList.length).toBe(1);
+        expect(doneList[0].description).toBe(list[1].description);
+        expect(doingList.length).toBe(3);
+    });
 });
